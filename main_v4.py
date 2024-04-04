f4e07d55fa8aa26862d62f7645a289655ce524d1
Victor <victorhugoarx@gmail.com>
2024-04-03 23:17:21 -0300

    certificado_auto_v4_final
    Tratei todas as exceções que pude pensar, adicionei o codigo a uma função, para melhor mobilidade e manutenção, assim como testei na prática.

---
+++ main_v4.py
@@ -0,0 +1,66 @@
+'''
+Pegar dados de uma planilha excel e passá-los para uma imagem de um certificado.
+Dados como: curso, nome, tipo da participação, data de início e final, carga horária e data de emissão.
+'''
+
+import openpyxl
+from PIL import Image, ImageDraw, ImageFont
+
+def gerar_certificados(planilha_path, imagem_certificado):
+    nomes_processados = []
+
+    try:
+        #Abrindo a planilha
+        planilha = openpyxl.load_workbook(planilha_path)
+        pag_planilha = planilha['Sheet1']
+
+        #Cada célula contém a info de cada 
+        for indice, coluna in enumerate(pag_planilha.iter_rows(min_row=2)):
+            if None in coluna:
+                print(f"Erro na linha{indice}: Valores em branco!")
+                continue
+
+            curso = coluna[0].value if coluna[0].value is not None else ""
+            nome = coluna[1].value if coluna[1].value is not None else ""
+            participacao = coluna[2].value if coluna[2].value is not None else ""
+            carga_horaria = coluna[5].value if coluna[3].value is not None else ""
+
+            data_inicio = coluna[3].value if coluna[4].value is not None else ""
+            data_final = coluna[4].value if coluna[5].value is not None else ""
+            
+            data_emissao = coluna[6].value if coluna[6].value is not None else ""
+
+            if nome in nomes_processados:
+                print(f"Erro na linha {indice + 1}: o nome já foi processado.")
+                continue
+            nomes_processados.append(nome)
+
+            #Tipos de Fontes
+            fonte_negrito = ImageFont.truetype('./tahomabd.ttf', 90)
+            fonte = ImageFont.truetype('./tahoma.ttf', 80)
+            fonte_data = ImageFont.truetype('./tahoma.ttf', 55)
+
+            #Abrindo Imagem
+            imagem = Image.open(imagem_certificado)
+            aplicando_texto = ImageDraw.Draw(imagem)
+
+            #Transferindo os dados da planilha para a imagem do certificado
+            aplicando_texto.text((1020, 827), nome, fill='black', font=fonte_negrito,)
+            aplicando_texto.text((1060, 950), curso, fill='black', font=fonte,)
+            aplicando_texto.text((1435, 1065), participacao, fill='black', font=fonte,)
+            aplicando_texto.text((1480, 1182), str(carga_horaria), fill='black', font=fonte,)
+
+            aplicando_texto.text((750, 1770), data_inicio, fill='black', font=fonte_data,)
+            aplicando_texto.text((750, 1930), data_final, fill='black', font=fonte_data,)
+
+            aplicando_texto.text((2220, 1930), data_emissao, fill='black', font=fonte_data,)
+
+
+            imagem.save(f'./[{indice + 1}]{nome}_[CERTIFICADO].jpg')
+
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {str(e)}")
+
+gerar_certificados(r'D:\Python(Emissão_Certificados)\planilha_alunos.xlsx', r'D:\Python(Emissão_Certificados)\certificado_padrao.jpg')
